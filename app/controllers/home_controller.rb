cdc1789bd36114a597f0d0b48ba3533e8ede65d9
Soll <artemzlat@mail.ru>
2015-07-30 00:57:32 +0500

    Style errors correct

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
 class HomeController < ApplicationController
-
   def index
     c = Card.all.where("review_date <= ?", Time.now).count
     @card = Card.offset(rand(c)).where("review_date <= ?", Time.now).first
@@ -7,16 +6,15 @@ class HomeController < ApplicationController
 
   def check_translation
     if params[:home][:cards][:original_text].mb_chars.downcase == params[:home][:cards][:review_text].mb_chars.downcase
-  	  @card = Card.find(params[:home][:cards][:id])
-  	  set_new_review_date
-  	  redirect_to cards_path
-  	else
-  		render "check_translation"
-  	end
+      @card = Card.find(params[:home][:cards][:id])
+      set_new_review_date
+      redirect_to cards_path
+    else
+      render "check_translation"
+    end
   end
 
   def set_new_review_date
     @card.update_column(:review_date, 3.days.from_now)
   end
-
 end
