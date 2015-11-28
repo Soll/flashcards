c3fedb5206c1e0e1668f091bd146c89a1d74b186
art <soll.sterben@gmail.com>
2015-11-28 12:21:13 +0500

    Task16

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,23 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :require_login, except: [:not_authenticated]
+  before_filter :set_locale
+
+  def set_locale
+    locale = if params[:locale]
+        session[:locale] = params[:locale]
+      elsif current_user
+        current_user.locale
+      elsif session[:locale]
+        session[:locale]
+      else
+        http_accept_language.compatible_language_from(I18n.available_locales)
+      end
+
+    if locale && I18n.available_locales.include?(locale.to_sym)
+      session[:locale] = I18n.locale = locale.to_sym
+    end
+  end
 
   protected
 
